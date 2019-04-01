89a364a2bf6eea43ca881bd1e00e1ec3bbe4e6b1
Tibor <tibor@seznam.cz>
2019-04-01 06:47:34 +0200

    add hub client project

---
+++ vendor/cable_client/client.rb
@@ -0,0 +1,37 @@
+#require '/utils/init'
+require 'action_cable_client'
+
+puts File.dirname(__dir__)
+puts File.expand_path(File.dirname(__dir__))
+
+EventMachine.run do
+
+  uri = "ws://192.168.225.15:3000/cable/"
+  client = ActionCableClient.new(uri, 'ServerInfoChannel', true, { "Origin" => "pokus.cz"})
+  # called whenever a welcome message is received from the server
+
+  client.subscribed do |m, n|
+    puts 'subscribed.'
+    puts m
+    client.perform('speak', { message: 'hello from ruby client 1.0.0.0, test hub app' })
+  end
+
+
+  client.connected do |m|
+    puts 'successfully connected.'
+    puts m
+  end
+
+  # called whenever a message is received from the server
+  client.received do | message |
+    puts message
+  end
+
+  client.pinged do |data|
+    puts data
+  end
+
+  # Sends a message to the sever, with the 'action', 'speak'
+  # client.perform('speak', { message: 'hello from amc' })
+
+end
