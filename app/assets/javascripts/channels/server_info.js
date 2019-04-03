cd8eb53b0dda0b92e59f6fde253d9f738c4464e0
tibor stuchlik <tibor@seznam.cz>
2019-04-03 10:22:32 -0400

    add authenticate with devise

--- app/assets/javascripts/channels/server_info.js
+++ app/assets/javascripts/channels/server_info.js
@@ -1,16 +1,19 @@
 App.server_info = App.cable.subscriptions.create("ServerInfoChannel", {
   connected: function() {
     // Called when the subscription is ready for use on the server
-    console.log("connected")
+    console.log("Connected to ServerInfoChannel")
+    //error = function(e) { console.log("Still Disconnected from ServerInfoChannel") }
+    os = $('#online_status')
+    os.html('(ONLINE)')
+    os.removeClass("text-danger").addClass("text-body")
   },
 
   disconnected: function() {
     // Called when the subscription has been terminated by the server
-  },
-
-  message: function() {
-
-      console.log("ping")
+      console.log("Disconnected from ServerInfoChannel")
+      os = $('#online_status')
+      os.html('(OFFLINE)')
+      os.removeClass("text-body").addClass("text-danger")
   },
 
   received: function(data) {
